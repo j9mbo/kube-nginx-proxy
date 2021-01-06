86eb67d2e4c68fdf1727c8cb1611354f77c3e87a
Vitalii Molnar <vitaliy.molnar@gmail.com>
2021-01-06 16:49:33 +0200

    added validation for env

--- env.sh
+++ env.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$ENV" != "production" ] && [ "$ENV" != "development" ]
+then
+echo wrong environment
+elif [ "$ENV" = "production" ] || [ "$ENV" = "development" ]
+then
 kubectl apply -f nginxdepl.yml -n=$ENV
 kubectl apply -f nginxlb.yml -n=$ENV
-	
+fi
